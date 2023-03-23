4d9ba045bb08abb60c45069384788e789f302b95
IsaaColomer <60975028+IsaaColomer@users.noreply.github.com>
2023-03-23 12:10:02 +0100

    polihbyg

--- TestAsd/Assets/PlayerController.cs
+++ TestAsd/Assets/PlayerController.cs
@@ -23,14 +23,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool performingAction;
     [SerializeField] private float xRotationAmmount;
     [SerializeField] private float yRotationAmmount;
-    [SerializeField] private float rotationSpeed;
-    [SerializeField] private float pSpeed;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform forwardStartPoint;
+    [SerializeField] private Transform upStartForwardPoint;
+    [SerializeField] private Transform upStartBackPoint;
+    public float forwardRaycastDistance;
+    [SerializeField] private Vector3 desiredMovePoint;
     private void Awake()
     {
         performingAction = false;
         pCamera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
         rb = GetComponent<Rigidbody>();
+        forwardStartPoint = GameObject.Find("RaycastForwardStartPoint").GetComponent<Transform>();
+        upStartForwardPoint = GameObject.Find("RaycastUpFrontStartPoint").GetComponent<Transform>();
+        upStartBackPoint = GameObject.Find("RaycastUpBackStartPoint").GetComponent<Transform>();
     }
     // Start is called before the first frame update
     void Start()
@@ -41,7 +47,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!performingAction)
+        RaycastForDebug();
+        if (!performingAction)
         {
             ResetActionsValues();
             if (Input.anyKeyDown)
@@ -62,18 +69,115 @@ public class PlayerController : MonoBehaviour
                 {
                     GetInputAction(KeyCode.D);
                 }
-                else if (Input.GetKeyDown(KeyCode.R))
-                {
-                    GetInputAction(KeyCode.R);
-                }
                 else if (Input.GetKeyDown(KeyCode.F))
                 {
                     GetInputAction(KeyCode.F);
+                }
+                else if (Input.GetKeyDown(KeyCode.R))
+                {
+                    GetInputAction(KeyCode.R);
                 }
             }
             
         }
         
+    }
+    void RaycastForDebug()
+    {
+        RaycastHit hit;
+        RaycastHit hit2;
+        if (!Physics.Raycast(forwardStartPoint.position, forwardStartPoint.forward, out hit, forwardRaycastDistance))
+        {
+            if (Physics.Raycast(upStartForwardPoint.position, -upStartForwardPoint.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                Debug.DrawLine(upStartForwardPoint.position, hit2.point, Color.green);
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+            }
+            
+        }
+        else
+        {
+            Debug.DrawLine(upStartForwardPoint.position, hit.point, Color.cyan);
+            if (Physics.Raycast(hit.point, -upStartBackPoint.transform.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                Debug.DrawLine(hit.point, hit2.point, Color.yellow);
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+            }
+        }
+    }
+    bool CanGoForward()
+    {
+        bool ret = false;
+        RaycastHit hit;
+        RaycastHit hit2;
+        if(!Physics.Raycast(forwardStartPoint.position, forwardStartPoint.forward, out hit, forwardRaycastDistance))
+        {
+            if(Physics.Raycast(upStartForwardPoint.position, -upStartForwardPoint.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                ret = true;
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+                ret = false;
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(hit.point, -upStartBackPoint.transform.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                ret = true;
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+                ret = false;
+            }
+        }
+        return ret;
+    }
+    bool CanGoBackward()
+    {
+        bool ret = false;
+        RaycastHit hit;
+        RaycastHit hit2;
+        if (!Physics.Raycast(forwardStartPoint.position, -forwardStartPoint.forward, out hit, forwardRaycastDistance))
+        {
+            if (Physics.Raycast(upStartBackPoint.position, -upStartBackPoint.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                ret = true;
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+                ret = false;
+            }
+        }
+        else
+        {
+            if (Physics.Raycast(hit.point, -upStartBackPoint.transform.up, out hit2, forwardRaycastDistance))
+            {
+                desiredMovePoint = hit2.point;
+                ret = true;
+            }
+            else
+            {
+                desiredMovePoint = Vector3.zero;
+                ret = false;
+            }
+        }
+        return ret;
     }
     public void GetInputAction(KeyCode key)
     {
@@ -82,42 +186,42 @@ public class PlayerController : MonoBehaviour
             case KeyCode.W:
                
                     performingAction = true;
-                    inputAction = Actions.MoveForward.ToString(); ;
+                    inputAction = Actions.MoveForward.ToString();
                     CheckInputAction();
                 
                 break;
             case KeyCode.S:
                 
                     performingAction = true;
-                    inputAction = Actions.MoveBackward.ToString(); ;
+                    inputAction = Actions.MoveBackward.ToString();
                     CheckInputAction();
                 
                 break;
             case KeyCode.D:
                 
                     performingAction = true;
-                    inputAction = Actions.TurnRight.ToString(); ;
+                    inputAction = Actions.TurnRight.ToString();
                     CheckInputAction();
                 
                 break;
             case KeyCode.A:
                 
                     performingAction = true;
-                    inputAction = Actions.TurnLeft.ToString(); ;
-                    CheckInputAction();
-                
-                break;
-            case KeyCode.R:
-                
-                    performingAction = true;
-                    inputAction = Actions.LookUp.ToString(); ;
+                    inputAction = Actions.TurnLeft.ToString();
                     CheckInputAction();
                 
                 break;
             case KeyCode.F:
                 
                     performingAction = true;
-                    inputAction = Actions.LookDown.ToString(); ;
+                    inputAction = Actions.LookUp.ToString();
+                    CheckInputAction();
+                
+                break;
+            case KeyCode.R:
+                
+                    performingAction = true;
+                    inputAction = Actions.LookDown.ToString();
                     CheckInputAction();
                 
                 break;
@@ -169,29 +273,41 @@ public class PlayerController : MonoBehaviour
                 performingAction = false;
                 break;
             case Actions.TurnLeft:
-                pCamera.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x, pCamera.transform.rotation.eulerAngles.y - xRotationAmmount, pCamera.transform.rotation.eulerAngles.z);
+                rb.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x, pCamera.transform.rotation.eulerAngles.y - xRotationAmmount, pCamera.transform.rotation.eulerAngles.z);
                 performingAction = false;
                 break;
             case Actions.TurnRight:
-                pCamera.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x, pCamera.transform.rotation.eulerAngles.y + xRotationAmmount, pCamera.transform.rotation.eulerAngles.z);
+                rb.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x, pCamera.transform.rotation.eulerAngles.y + xRotationAmmount, pCamera.transform.rotation.eulerAngles.z);
                 performingAction = false;
                 break;
             case Actions.MoveForward:
-                rb.velocity = pCamera.transform.forward * pSpeed;
+                if(CanGoForward() && desiredMovePoint != Vector3.zero)
+                {
+                    rb.transform.position = new Vector3(desiredMovePoint.x, desiredMovePoint.y + 0.71f, desiredMovePoint.z);
+                }
                 performingAction = false;
                 break;
             case Actions.MoveBackward:
-                rb.velocity = -pCamera.transform.forward * pSpeed;
+                if (CanGoBackward() && desiredMovePoint != Vector3.zero)
+                {
+                    rb.transform.position = new Vector3(desiredMovePoint.x, desiredMovePoint.y + 0.71f, desiredMovePoint.z) ;
+                }
                 performingAction = false;
                 break;
             case Actions.LookDown:
-                    pCamera.transform.rotation = Quaternion.Euler(Mathf.Clamp(pCamera.transform.rotation.eulerAngles.x, -60, 60) + yRotationAmmount, pCamera.transform.rotation.eulerAngles.y, pCamera.transform.rotation.eulerAngles.z);
-                    performingAction = false;
-                
+                if ((int)pCamera.transform.rotation.eulerAngles.x == 0 || (int)pCamera.transform.rotation.eulerAngles.x == 330)
+                {
+                    pCamera.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x - yRotationAmmount, pCamera.transform.rotation.eulerAngles.y, pCamera.transform.rotation.eulerAngles.z);
+                }
+                performingAction = false;
+                 
                 break;
             case Actions.LookUp:
-                    pCamera.transform.rotation = Quaternion.Euler(Mathf.Clamp(pCamera.transform.rotation.eulerAngles.x, -60, 60) - yRotationAmmount, pCamera.transform.rotation.eulerAngles.y, pCamera.transform.rotation.eulerAngles.z);
-                    performingAction = false;
+                if ((int)pCamera.transform.rotation.eulerAngles.x == 0 || (int)pCamera.transform.localRotation.eulerAngles.x == 30)
+                {
+                    pCamera.transform.rotation = Quaternion.Euler(pCamera.transform.rotation.eulerAngles.x + yRotationAmmount, pCamera.transform.rotation.eulerAngles.y, pCamera.transform.rotation.eulerAngles.z);
+                }
+                performingAction = false;
                 break;
             default:
                 break;
